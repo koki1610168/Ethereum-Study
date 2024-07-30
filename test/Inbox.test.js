73586220e7150967a151c52d2ee2a5cb20f5a291
koki1610168 <k.yahata1610168@gmail.com>
2024-07-30 06:43:29 -0500

    modify test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,19 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
-
+const assert = require('assert');
+const { describe, before } = require('mocha');
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+});
+
+describe('Inbox', () => {
+    it('deploys', () => {
+        console.log(accounts);
+    });
+});
